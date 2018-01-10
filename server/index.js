32efa1686ca15b573435a474cefc9a002d17f4b1
Mohmed Bamhraz <mohmedbamhrz@gmail.com>
2018-01-10 21:49:18 +0300

    add oauth

--- server/index.js
+++ server/index.js
@@ -1,10 +1,11 @@
 const express = require('express')
+const passport = require('passport')
+const GoogleStrategy = require('passport-google-oauth20').Strategy
 const app = express()
 
-app.get('/', (req, res) => {
-  res.send({ buy: 'bidd' })
-})
+passport.use(new GoogleStrategy())
 
-
+// client_id 172871001145-fvsbp6hqaola2d7i32a2oipugva7lvr4.apps.googleusercontent.com
+// client_secret qK4E4VcXfT3VZ8ROdkXQac73
 const PORT = process.env.PORT || 5000
 app.listen(PORT)

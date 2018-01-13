f020232c83cb66be48e66c3bd7c85fe1745e5afd
Mohmed Bamhraz <mohmedbamhrz@gmail.com>
2018-01-13 17:38:51 +0300

    - linking in mongoDB

--- server/index.js
+++ server/index.js
@@ -1,6 +1,11 @@
 const express = require('express')
+const mongoose = require('mongoose')
+const keys = require('./config/keys')
 const app = express()
+require('./models/User')
 require('./services/passport')
+
+mongoose.connect(keys.mongoURI)
 
 require('./routes/authRoutes')(app);
 

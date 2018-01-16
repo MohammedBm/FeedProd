13e0ee832d98359aeb1fbf6633ac16f4db2edadb
Mohmed Bamhraz <mohmedbamhrz@gmail.com>
2018-01-16 22:06:09 +0300

    - use aysnc function

--- server/services/passport.js
+++ server/services/passport.js
@@ -23,21 +23,18 @@ passport.use(
     clientSecret: keys.googleClientSecret, // this is the secret key we got from google+ oauth
     callbackURL: '/auth/google/callback',
     proxy: true
-  }, (acessToken, refreshToken, profile, done) => {
-    User.findOne({
+  },
+  async(acessToken, refreshToken, profile, done) => {
+    const existingUser = await User.findOne({
       googleId: profile.id
-    }).then((existingUser) => {
-      if (existingUser) {
-        // we already have a record with the given profile id
-        done(null, existingUser)
-      } else {
-        // we dont have a user with this ID
-        new User({
-            googleId: profile.id
-          })
-          .save()
-          .then(user => done(null, user))
-      }
     })
+
+    if (existingUser) {
+      // we already have a record with the given profile id
+      return done(null, existingUser)
+    }
+      // we dont have a user with this ID
+      const user = await new User({ googleId: profile.id }).save()
+      done(null, user)
   })
 )

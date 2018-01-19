7fa3cd1a5861c26e4c15f602b0b495b56767e90a
Mohmed Bamhraz <mohmedbamhrz@gmail.com>
2018-01-19 20:41:52 +0300

    - add materialize-css

---
+++ server/client/src/components/Header.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Header extends Component {
+  render() {
+    return (
+      <h4>
+        Header
+      </h4>
+    )
+  }
+}
+
+export default Header;

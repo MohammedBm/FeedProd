8694cf94b689590c878b6cf564c159277aae3188
Mohmed Bamhraz <mohmedbamhrz@gmail.com>
2018-01-31 19:50:22 +0300

    - update prod.js  - add a new key  - updat surveyTemplate.js

--- server/routes/surveyRoutes.js
+++ server/routes/surveyRoutes.js
@@ -4,7 +4,12 @@ const requireCredits = require("../middlewares/requireCredits");
 const Mailer = require("../services/Mailer");
 const surveyTemplate = require("../services/emailTemplates/surveyTemplate");
 const Survey = mongoose.model("surveys");
+
 module.exports = app => {
+  app.get('/api/surveys/thanks', (req, res) => {
+    res.send('Thanks for voting')
+  })
+
   app.post("/api/surveys", requireLogin, requireCredits, async (req, res) => {
     const { title, subject, body, recipients } = req.body;
     console.log('------------------------------------');

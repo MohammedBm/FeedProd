c99e78d7894d3a1921771e83f3617053d44a092f
Mohmed Bamhraz <mohmedbamhrz@gmail.com>
2018-01-31 18:34:19 +0300

    - rewrite some part of the code  - fix the issue where the user wasn't able to get email from sendgrid

--- server/routes/surveyRoutes.js
+++ server/routes/surveyRoutes.js
@@ -1,20 +1,33 @@
-const mongoose = require('mongoose');
-const requireLogin = require('../middlewares/requireLogin');
-const requireCredits = require('../middlewares/requireCredits');
-
-const Survey = mongoose.model('surveys');
-
+const mongoose = require("mongoose");
+const requireLogin = require("../middlewares/requireLogin");
+const requireCredits = require("../middlewares/requireCredits");
+const Mailer = require("../services/Mailer");
+const surveyTemplate = require("../services/emailTemplates/surveyTemplate");
+const Survey = mongoose.model("surveys");
 module.exports = app => {
-  app.post('/api/surveys', requireLogin, requireCredits, (req, res) => {
+  app.post("/api/surveys", requireLogin, requireCredits, async (req, res) => {
     const { title, subject, body, recipients } = req.body;
-
+    console.log('------------------------------------');
+    console.log(req.body);
+    console.log('------------------------------------');
     const survey = new Survey({
       title,
       subject,
       body,
-      recipients: recipients.split(',').map(email => ({ email })),
+      recipients: recipients.split(",").map(email => ({ email: email.trim() })),
       _user: req.user.id,
       dateSent: Date.now()
     });
+    //Great place to send an email
+    const mailer = new Mailer(survey, surveyTemplate(survey));
+    try {
+      await mailer.send();
+      await survey.save();
+      req.user.credits -= 1;
+      const user = await req.user.save();
+      res.send(user);
+    } catch (err) {
+      res.status(422).send(err);
+    }
   });
 };
--- server/services/emailTemplate/surveyTemplate.js
+++
@@ -1,3 +0,0 @@
-module.exports = survey => {
-  return '<div>Hello There!</div>' + survey.body + '<div>Mohammed</div>'
-}
